9932acf9c8afaf74d2328cd489f160f2f30b1de7
Juan Villela <juan@villela.co>
2021-02-21 18:29:42 -0500

    chore: Rename favicon script to images.
    This script will become the make image pipeline script. It will handle favicons, logo, and any post images.

--- scripts/images.ts
+++ scripts/images.ts
@@ -1,32 +1,38 @@
 import { resolve } from 'path';
 import sharp from 'sharp';
 
-interface Variants {
+interface VOptions {
   name: string;
   size: number;
 }
 
-const variants: Variants[] = [
-  { name: 'favicon', size: 16 },
-  { name: 'favicon', size: 32 },
-  { name: 'icon', size: 120 },
-  { name: 'icon', size: 180 },
-  { name: 'icon', size: 152 },
-  { name: 'icon', size: 167 },
-  { name: 'icon', size: 256 },
-  { name: 'icon', size: 512 },
-];
+interface Variants {
+  favicon: VOptions[];
+}
+
+const variants: Variants = {
+  favicon: [
+    { name: 'favicon', size: 16 },
+    { name: 'favicon', size: 32 },
+    { name: 'icon', size: 120 },
+    { name: 'icon', size: 180 },
+    { name: 'icon', size: 152 },
+    { name: 'icon', size: 167 },
+    { name: 'icon', size: 256 },
+    { name: 'icon', size: 512 },
+  ],
+};
 const dir = resolve(__dirname, '..', 'public/icons');
 
 const createIcons = async () => {
   // map array to promises
-  const promises = variants.map(async img => {
+  const promises = variants.favicon.map(async img => {
     const fileName = `${img.name}-${img.size}x${img.size}.png`;
 
     await sharp('./favicon.png')
       .resize(img.size, img.size)
       .toFile(`${dir}/${fileName}`)
-      .then(info => {
+      .then(() => {
         console.info(`${fileName} created.`);
       })
       .catch(err => {
