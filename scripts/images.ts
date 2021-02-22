3f56db5d46bf6254c9763c3c512bf0d4ab19003f
Juan Villela <juan@villela.co>
2021-02-21 19:05:44 -0500

    chore: Update image script main function for reusability.

--- scripts/images.ts
+++ scripts/images.ts
@@ -3,40 +3,66 @@ import sharp from 'sharp';
 
 interface VOptions {
   name: string;
-  size: number;
+  resize: number | number[];
+  ext?: string;
 }
 
 interface Variants {
   favicon: VOptions[];
 }
 
+interface ResizeSizes {
+  height?: string;
+  width: string;
+}
+
 const variants: Variants = {
   favicon: [
-    { name: 'favicon', size: 16 },
-    { name: 'favicon', size: 32 },
-    { name: 'icon', size: 120 },
-    { name: 'icon', size: 180 },
-    { name: 'icon', size: 152 },
-    { name: 'icon', size: 167 },
-    { name: 'icon', size: 256 },
-    { name: 'icon', size: 512 },
+    { name: 'favicon', resize: [16, 16] },
+    { name: 'favicon', resize: [32, 32] },
+    { name: 'icon', resize: [120, 120] },
+    { name: 'icon', resize: [180, 180] },
+    { name: 'icon', resize: [152, 152] },
+    { name: 'icon', resize: [167, 167] },
+    { name: 'icon', resize: [256, 256] },
+    { name: 'icon', resize: [512, 512] },
   ],
 };
-const dir = resolve(__dirname, '..', 'public/icons');
+const dir = resolve(__dirname, '..', 'public');
 
-const createIcons = async () => {
+/**
+ * Create various size and format variants of an image.
+ * @function
+ *
+ * @param {string} src  Image to format and convert
+ * @param {string} dest Where to save image
+ */
+const fmtImage = async (src: string, dest: string): Promise<void> => {
+  const input = resolve(__dirname, src);
+
   // map array to promises
   const promises = variants.favicon.map(async img => {
-    const fileName = `${img.name}-${img.size}x${img.size}.png`;
+    // image options
+    const sizes = Array.isArray(img.resize)
+      ? { height: img.resize[0], width: img.resize[1] }
+      : { width: img.resize };
+    const type: any = img.ext || 'png';
+    const fileName: string = Array.isArray(img.resize)
+      ? `${img.name}-${img.resize[0]}x${img.resize[1]}.${type}`
+      : `${img.name}.${type}`;
+    const output = `${dir}/${dest}/${fileName}`;
+    const image = await sharp(input);
 
-    await sharp('./favicon.png')
-      .resize(img.size, img.size)
-      .toFile(`${dir}/${fileName}`)
+    // create variants
+    await image
+      .resize(sizes)
+      .toFormat(type)
+      .toFile(output)
       .then(() => {
         console.info(`${fileName} created.`);
       })
       .catch(err => {
-        console.error(`Sharp Error (${fileName}):`, err);
+        console.error(`Sharp Error:`, { input, output, err });
       });
   });
 
@@ -44,4 +70,4 @@ const createIcons = async () => {
   await Promise.all(promises);
 };
 
-createIcons();
+fmtImage('favicon.png', 'icons');
