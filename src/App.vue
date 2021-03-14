53457342de3d6a7f6d3cec24fa321d0d2852f953
Juan Villela <juan@villela.co>
2021-03-14 00:47:42 -0500

    feat: Add main App component.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <main></main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+});
+</script>
+
+<style></style>
