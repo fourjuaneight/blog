fac1bd7ba38b172dbaffc1db92c78d8cf48318b6
Juan Villela <juan@villela.co>
2022-03-10 17:47:41 -0500

    feat: Add bookmarks data saving script.

---
+++ scripts/bookmarks-data.ts
@@ -0,0 +1,134 @@
+import { promises } from 'fs';
+import { resolve } from 'path';
+
+import chalk from 'chalk';
+import dotenv from 'dotenv';
+import fetch from 'isomorphic-fetch';
+
+import {
+  AirtableBKResp,
+  BookmarksData,
+  BKFields,
+  BKRecord,
+  RedditFields,
+  TweetFields,
+  WebFields,
+} from './types';
+
+dotenv.config();
+
+const dist = resolve(__dirname, '..', 'data');
+const { writeFile } = promises;
+const { AIRTABLE_BOOKMARKS_ID, AIRTABLE_TOKEN } = process.env;
+// table data
+const data: BookmarksData = {
+  Articles: [],
+  Comics: [],
+  Podcasts: [],
+  Reddits: [],
+  Tweets: [],
+  Videos: [],
+};
+
+/**
+ * Get bookmarks list from Airtable.
+ * Request can be recursive is there is more than 100 records.
+ * @function
+ * @async
+ *
+ * @param  {string}   table  table name
+ * @param  {[string]} offset param to request remainding records
+ * @return {AirtableBKResp}
+ */
+const getBookmarksWithOffset = (
+  table: string,
+  offset?: string
+): Promise<AirtableBKResp> => {
+  const options: RequestInit = {
+    headers: {
+      Authorization: `Bearer ${AIRTABLE_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+  };
+  const endpoint = `https://api.airtable.com/v0/${AIRTABLE_BOOKMARKS_ID}/${table}`;
+  const url = offset ? `${endpoint}?offset=${offset}` : endpoint;
+
+  try {
+    return fetch(url, options)
+      .then((response: Response) => response.json())
+      .then((airtableRes: AirtableBKResp) => {
+        const existingData = data[table] || [];
+        const newData =
+          airtableRes.records?.map(record => {
+            delete record.fields.archive;
+            return record.fields;
+          }) || [];
+
+        data[table] = [...existingData, ...newData];
+
+        if (airtableRes.offset) {
+          return getBookmarksWithOffset(airtableRes.offset);
+        }
+
+        return airtableRes;
+      });
+  } catch (error) {
+    throw `${chalk.red('[ERROR]')} ${chalk.blue(
+      '(getBookmarksWithOffset)'
+    )} (${table.toLowerCase()}) - ${error}`;
+  }
+};
+
+/**
+ * Save Airtable data to local JSON data file.
+ * @function
+ * @async
+ *
+ * @param  {string} table table name
+ * @return {void}
+ */
+const saveData = async (table: string): Promise<void> => {
+  const tableName = table.toLowerCase();
+
+  try {
+    await getBookmarksWithOffset(table);
+
+    if (data[table]?.length) {
+      await writeFile(
+        `${dist}/bookmarks-${tableName}.json`,
+        JSON.stringify(data[table], null, 2)
+      );
+
+      console.info(
+        chalk.green('[SUCCESS]'),
+        `${chalk.blue(
+          '(saveData)'
+        )} - Data saved to bookmarks-${tableName}.json`
+      );
+    } else {
+      console.info(
+        chalk.yellow('[INFO]'),
+        `${chalk.blue(
+          '(saveData)'
+        )} - No data to save to bookmarks-${tableName}.json`
+      );
+    }
+  } catch (error) {
+    throw `${chalk.red('[ERROR]')} ${chalk.blue(
+      '(saveData)'
+    )} (${tableName}) - ${error}`;
+  }
+};
+
+(async () => {
+  try {
+    const dataList = Object.keys(data).map(table => saveData(table));
+
+    await Promise.all(dataList);
+
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
--- scripts/types.ts
+++ scripts/types.ts
@@ -1,3 +1,53 @@
+// Bookmarks
+export interface RedditFields {
+  archive?: string;
+  content: string;
+  date: string;
+  subreddit: string;
+  tags: string[];
+  title: string;
+  url: string;
+}
+
+export interface TweetFields {
+  archive?: string;
+  creator: string;
+  tweet: string;
+  url: string;
+  tags: string[];
+}
+
+export interface WebFields {
+  archive?: string;
+  creator: string;
+  title: string;
+  url: string;
+  tags: string[];
+}
+
+export type BKFields = RedditFields | TweetFields | WebFields;
+
+export interface BKRecord {
+  id: string;
+  fields: BKFields;
+  createdTime: string;
+}
+
+export interface AirtableBKResp {
+  records: BKRecord[];
+  offset: string;
+}
+
+export interface BookmarksData {
+  Articles: BKFields[];
+  Comics: BKFields[];
+  Podcasts: BKFields[];
+  Reddits: BKFields[];
+  Tweets: BKFields[];
+  Videos: BKFields[];
+  [key: string]: BKFields[];
+}
+
 // Shelf
 export interface ShelfFields {
   name: string;
