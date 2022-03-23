a47b2eccff0d6bfd58a53a88d266a8333dd35fe5
Juan Villela <juan@villela.co>
2022-03-23 18:11:27 -0400

    feat: Update data fetch method to use page function.

--- scripts/bookmarks-data.ts
+++ scripts/bookmarks-data.ts
@@ -2,11 +2,9 @@ import { promises } from 'fs';
 import { resolve } from 'path';
 
 import chalk from 'chalk';
-import dotenv from 'dotenv';
 import fetch from 'isomorphic-fetch';
 
 import {
-  AirtableBKResp,
   BookmarksData,
   BKFields,
   BKRecord,
@@ -16,11 +14,8 @@ import {
   WebFields,
 } from './types';
 
-dotenv.config();
-
 const dist = resolve(__dirname, '..', 'data');
 const { writeFile } = promises;
-const { AIRTABLE_BOOKMARKS_ID, AIRTABLE_TOKEN } = process.env;
 // table data
 const data: BookmarksData = {
   Articles: [],
@@ -31,60 +26,24 @@ const data: BookmarksData = {
   Videos: [],
 };
 
-/**
- * Get bookmarks list from Airtable.
- * Request can be recursive is there is more than 100 records.
- * @function
- * @async
- *
- * @param  {string}   table  table name
- * @param  {[string]} offset param to request remainding records
- * @return {AirtableBKResp}
- */
-const getBookmarksWithOffset = async (
-  table: string,
-  offset?: string
-): Promise<AirtableBKResp> => {
-  const options: RequestInit = {
-    headers: {
-      Authorization: `Bearer ${AIRTABLE_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-  };
-  const endpoint = `https://api.airtable.com/v0/${AIRTABLE_BOOKMARKS_ID}/${table}`;
-  const url = offset ? `${endpoint}?offset=${offset}` : endpoint;
-
+const getBookmarks = async (table: string): Promise<void> => {
   try {
-    return fetch(url, options)
-      .then((response: Response) => response.json())
-      .then((airtableRes: AirtableBKResp) => {
-        const existingData = data[table] || [];
-        const newData =
-          airtableRes.records?.map(record => {
-            delete record.fields.archive;
-            return { id: record.id, ...record.fields };
-          }) || [];
+    const response = await fetch(
+      `https://cleverlaziness.com/api/bookmarks/${table}`
+    );
+    const bookmarks: BKValues[] = await response.json();
 
-        data[table] = [...existingData, ...newData];
-
-        if (airtableRes.offset) {
-          return getBookmarksWithOffset(airtableRes.offset);
-        }
-
-        return airtableRes;
-      });
+    data[table] = bookmarks;
   } catch (error) {
     throw `${chalk.red('[ERROR]')} ${chalk.blue(
-      '(getBookmarksWithOffset)'
+      '(getBookmarks)'
     )} (${table.toLowerCase()}) ${error}`;
   }
 };
 
 (async () => {
   try {
-    const dataList = Object.keys(data).map(table =>
-      getBookmarksWithOffset(table)
-    );
+    const dataList = Object.keys(data).map(table => getBookmarks(table));
 
     await Promise.all(dataList);
 
