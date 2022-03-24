aa44727d3d3ffa8692ecefdc8fa769300b5a5676
Juan Villela <juan@villela.co>
2022-03-24 16:20:34 -0400

    feat: Use page function to request shelf data.

--- scripts/shelf-data.ts
+++ scripts/shelf-data.ts
@@ -2,103 +2,41 @@ import { promises } from 'fs';
 import { resolve } from 'path';
 
 import chalk from 'chalk';
-import dotenv from 'dotenv';
 import fetch from 'isomorphic-fetch';
 
-import {
-  AirtableShelfResp,
-  ShelfFields,
-  ShelfItem,
-  ShelfRecord,
-} from './types';
+import { ShelfItem } from './types';
 
-dotenv.config();
+interface ShelfData {
+  [key: string]: ShelfItem[];
+}
 
 const dist = resolve(__dirname, '..', 'data');
 const { writeFile } = promises;
-const { AIRTABLE_MEDIA_ID, AIRTABLE_TOKEN } = process.env;
-let data: ShelfRecord[] = [];
+let data: ShelfData = {};
 
-/**
- * Get shelf list from Airtable.
- * Request can be recursive is there is more than 100 records.
- * @function
- * @async
- *
- * @param  {[string]} offset param to request remainding records
- * @return {AirtableShelfResp}
- */
-const getShelfWithOffset = (offset?: string): Promise<AirtableShelfResp> => {
-  const options: RequestInit = {
-    headers: {
-      Authorization: `Bearer ${AIRTABLE_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-  };
-  const endpoint = `https://api.airtable.com/v0/${AIRTABLE_MEDIA_ID}/Shelf`;
-  const url = offset ? `${endpoint}?offset=${offset}` : endpoint;
-
+const getShelf = async (): Promise<void> => {
   try {
-    return fetch(url, options)
-      .then((response: Response) => response.json())
-      .then((airtableRes: AirtableShelfResp) => {
-        data = [...data, ...airtableRes.records];
+    const response = await fetch('https://cleverlaziness.com/api/shelf/');
+    const shelf: ShelfData = await response.json();
 
-        if (airtableRes.offset) {
-          return getShelfWithOffset(airtableRes.offset);
-        }
-        return airtableRes;
-      });
+    data = shelf;
   } catch (error) {
-    throw `${chalk.red('[ERROR]')} ${chalk.blue(
-      '(getShelfWithOffset)'
-    )} ${error}`;
+    throw `${chalk.red('[ERROR]')} ${chalk.blue('(getShelf)')} ${error}`;
   }
 };
 
-/**
- * Save Airtable data to local JSON data file.
- * @function
- * @async
- *
- * @return {void}
- */
 const saveData = async (): Promise<void> => {
   try {
-    await getShelfWithOffset();
+    await getShelf();
 
-    if (data.length) {
-      const cleanData: ShelfItem[] = data
-        .map((item: ShelfRecord) => ({
-          id: item.id,
-          ...item.fields,
-          cover: item.fields.cover[0].url,
-          completed: item.fields.completed || false,
-        }))
-        .sort((a: ShelfItem, b: ShelfItem) => a.name.localeCompare(b.name));
-      const groupedData: { [key: string]: ShelfItem[] } = cleanData.reduce(
-        (acc: { [key: string]: ShelfItem[] }, item: ShelfItem) => {
-          const key = item.category;
+    const isDataEmpty = Object.keys(data).length === 0;
 
-          if (!acc[key]) {
-            acc[key] = [];
-          }
-
-          acc[key].push(item);
-
-          return acc;
-        },
-        {}
-      );
-
-      await writeFile(
-        `${dist}/shelf.json`,
-        JSON.stringify(groupedData, null, 2)
-      );
+    if (!isDataEmpty) {
+      await writeFile(`${dist}/shelf.json`, JSON.stringify(data, null, 2));
 
       console.info(chalk.green('[SUCCESS]'), 'data saved to shelf.json');
     } else {
-      console.info(chalk.yellow('[INFO]'), 'no data saved to shelf.json');
+      console.info(chalk.yellow('[SUCCESS]'), 'no data saved to shelf.json');
     }
   } catch (error) {
     throw `${chalk.red('[ERROR]')} ${chalk.blue('(saveData)')} ${error}`;
