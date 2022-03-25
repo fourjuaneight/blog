b3be05ef010c4af783f86cb7c914a0dc23ec3165
Juan Villela <juan@villela.co>
2022-03-25 16:04:23 -0400

    feat: Add bookmarks dom generation and db upgrades to worker.

--- assets/workers/bookmarks.ts
+++ assets/workers/bookmarks.ts
@@ -1,11 +1,102 @@
+import { BKValues } from '../../scripts/types';
+
+interface MessageValues {
+  bookmarks: BKValues[];
+  isExpired: boolean;
+}
+
 const worker = new Worker('/bookmarks.ww.min.js');
 const body = document.querySelector('body');
 const tableName = body.getAttribute('data-table');
+const tableList = document.querySelector(
+  `ul[data-category="${tableName?.toLowerCase()}"]`
+);
+const fragment = document.createDocumentFragment();
+
+const loadBookmarkToDOM = bookmark => {
+  // create element
+  const wrap = document.createElement('li');
+  const title = document.createElement('a');
+  const creator = document.createElement('p');
+  const tags = document.createElement('p');
+
+  // set attributes
+  wrap.classList.add(
+    'bk-cell',
+    'border-solid',
+    'duration-200',
+    'ease-in-out',
+    'grid',
+    'grid-rows-1',
+    'm-0',
+    'p-0',
+    'transition-all',
+    'w-full'
+  );
+  title.classList.add(
+    'col-start-1',
+    'focus:no-underline',
+    'hove:no-underline',
+    'm-0',
+    'no-underline',
+    'p-0',
+    'px-2',
+    'w-full',
+    'whitespace-nowrap'
+  );
+  title.setAttribute('title', bookmark.title);
+  title.setAttribute('href', bookmark.url);
+  title.setAttribute('target', '_blank');
+  title.setAttribute('rel', 'noopener noreferrer');
+  creator.classList.add('col-start-2', 'm-0', 'p-0', 'px-2', 'w-full');
+  tags.classList.add(
+    'col-start-3',
+    'm-0',
+    'overflow-hidden',
+    'p-0',
+    'px-2',
+    'w-full'
+  );
+
+  // add content
+  title.textContent = bookmark.title || bookmark.tweet;
+  creator.textContent = bookmark.creator || bookmark.subreddit;
+  tags.textContent = bookmark.tags.join(', ');
+
+  // add to fragment
+  wrap.appendChild(title);
+  wrap.appendChild(creator);
+  wrap.appendChild(tags);
+  fragment.appendChild(wrap);
+};
 
 if (window.Worker) {
-  worker.postMessage(tableName);
+  const dbVersion = localStorage.getItem('Bookmarks');
+  const tableVersion = localStorage.getItem(tableName);
 
-  worker.onmessage = (evt: MessageEvent<string>) => {
-    localStorage.setItem(tableName, evt.data);
+  worker.postMessage({
+    dbVersion: dbVersion ? parseInt(dbVersion) : 1,
+    table: {
+      name: tableName,
+      version: tableVersion ? parseInt(tableVersion) : Date.now(),
+      new: !tableVersion,
+    },
+  });
+
+  worker.onmessage = (evt: MessageEvent<MessageValues>) => {
+    // generate bookmark elements
+    for (const item of evt.data.bookmarks) {
+      loadBookmarkToDOM(item);
+    }
+
+    // add to DOM
+    tableList.appendChild(fragment);
+
+    // set db and table versions
+    localStorage.setItem('Bookmarks', evt.data.dbVersion);
+
+    if (evt.data.isExpired) {
+      localStorage.setItem(tableName, Date.now());
+    }
   };
 }
--- assets/workers/bookmarks.ww.ts
+++ assets/workers/bookmarks.ww.ts
@@ -1,6 +1,18 @@
 import { openDB, DBSchema } from 'idb';
+import { addDays, compareAsc, getUnixTime } from 'date-fns';
 
 import { BKValues } from '../../scripts/types';
+
+interface TableValues {
+  name: string;
+  version: number;
+  new: boolean;
+}
+
+interface MessageValues {
+  dbVersion: number;
+  table: TableValues;
+}
 
 interface BookmarksDB extends DBSchema {
   [key: string]: {
@@ -9,31 +21,53 @@ interface BookmarksDB extends DBSchema {
   };
 }
 
-const saveBookmarksToStore = async (table: string) => {
-  postMessage('loading');
+const getBookmarksFromStore = async (
+  tableName: string,
+  isExpired: boolean,
+  dbVersion: number
+): Promise<void> => {
+  // open store and get bookmakrs
+  const store = await openDB<BookmarksDB>('Bookmarks');
+  const bookmarks = await store.getAll(tableName);
 
-  // create store
-  const tableName = table.toLowerCase();
-  const store = await openDB<BookmarksDB>(`bookmarks`, 1, {
-    upgrade(db) {
-      db.createObjectStore(tableName, {
+  // send bookmarks to main thread
+  postMessage({ bookmarks, isExpired, dbVersion });
+};
+
+const saveBookmarksToStore = async (dbVersion: number, table: TableValues) => {
+  // determine if bookmarks are expired
+  const currentTime = new Date();
+  const versionTime = addDays(table.version, 1);
+  const difference = compareAsc(currentTime, versionTime);
+  const isExpired = table.new ? true : difference === 1;
+  const newDBVersion = isExpired ? dbVersion + 1 : dbVersion;
+
+  // create new store
+  console.info({ dbVersion, table, isExpired, newDBVersion });
+  const store = await openDB<BookmarksDB>(`Bookmarks`, newDBVersion, {
+    upgrade(db, dbVersion, newVersion) {
+      db.createObjectStore(table.name, {
         keyPath: 'id',
       });
     },
   });
-  // get bookmarks
-  const response = await fetch(
-    `https://cleverlaziness.com/api/bookmarks/${table}`
-  );
-  const data: BKValues[] = await response.json();
 
-  // add bookmarks to store
-  const storeUpdate = data.map(bookmark => store.put(tableName, bookmark));
-  await Promise.all(storeUpdate);
+  if (isExpired) {
+    // get bookmarks
+    const response = await fetch(
+      `https://cleverlaziness.com/api/bookmarks/${table.name}`
+    );
+    const data: BKValues[] = await response.json();
 
-  postMessage('loaded');
+    // add bookmarks to store
+    const storeUpdate = data.map(bookmark => store.put(table.name, bookmark));
+    await Promise.all(storeUpdate);
+  }
+
+  // get bookmarks and send to main thread
+  await getBookmarksFromStore(table.name, isExpired, newDBVersion);
 };
 
-onmessage = evt => {
-  saveBookmarksToStore(evt.data);
+onmessage = (evt: MessageEvent<MessageValues>) => {
+  saveBookmarksToStore(evt.data.dbVersion, evt.data.table);
 };
