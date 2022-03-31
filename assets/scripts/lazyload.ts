78bed108b55265a1bc5368f67be39873a4a5546d
Juan Villela <juan@villela.co>
2022-03-31 16:52:44 -0400

    feat: Add img lazy loading site wide.

--- assets/scripts/lazyload.ts
+++ assets/scripts/lazyload.ts
@@ -6,9 +6,14 @@ let observer = new IntersectionObserver((entries, self) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       const img = entry.target as HTMLImageElement;
+      const hasSet = img.getAttribute('data-srcset');
 
+      if (hasSet) {
+        img.srcset = img.dataset.srcset;
+      }
       img.src = img.dataset.src;
       img.removeAttribute('data-lazy');
+      img.classList.remove('opacity-0');
 
       self.unobserve(img);
     }
