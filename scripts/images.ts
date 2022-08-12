cbed797b70b20be6656fb55b1017bd96d3e993c2
Juan Villela <juan@villela.co>
2022-08-11 20:26:46 -0400

    chore: Remove deprecated method.

--- scripts/images.ts
+++ scripts/images.ts
@@ -34,68 +34,6 @@ const icons: Icons[] = [
 ];
 
 /**
- * Create placeholder image.
- * @function
- * @async
- *
- * @param {string} src Image to format and convert
- *
- * @returns {Promise}
- */
-const fmtPlaceholder = async (src: string): Promise<void> => {
-  const input = resolve(dir, src);
-  const name = src.replace(/([a-zA-Z_]+).png/g, '$1');
-  // format options
-  const fmtOps: FormatOptions = {
-    png: {
-      progressive: true,
-      quality: 20,
-      compressionLevel: 9,
-    },
-    web: {
-      effort: 6,
-      quality: 20,
-    },
-    avif: {
-      effort: 6,
-      quality: 20,
-    },
-  };
-  // image options
-  const image = sharp(input);
-  const resizeOps: sharp.ResizeOptions = {
-    background: { r: 255, g: 255, b: 255, alpha: 0.0 },
-    fit: 'contain',
-  };
-
-  try {
-    // create variants
-    const buff = await image.resize(64, undefined, resizeOps).blur().toBuffer();
-    const promises = variants.map(async ext => {
-      await sharp(buff)
-        .resize(1024, undefined, resizeOps)
-
-        .toFormat(ext as any, fmtOps[ext])
-        .toFile(`${imgDist}/${name}-placeholder.${ext}`);
-    });
-
-    await Promise.all(promises);
-
-    logger.info(`[images][fmtPlaceholder]: ${name} placeholder created`);
-  } catch (err) {
-    throw `${JSON.stringify(
-      {
-        method: 'fmtPlaceholder',
-        name,
-        err,
-      },
-      undefined,
-      2
-    )}`;
-  }
-};
-
-/**
  * Create various size and format variants of an image.
  * @function
  * @async
@@ -187,7 +125,6 @@ const fmtIcon = async (name: string, size: number): Promise<void> => {
   try {
     const imgFiles = globSync('*.png', { cwd: dir });
     const iconOps = icons.map(icon => fmtIcon(icon.name, icon.size));
-    const imgPHOps = imgFiles.map(file => fmtPlaceholder(file));
     const imgOps = imgFiles.map(file => fmtImage(file));
     const ops = [...iconOps, ...imgOps];
 
