54ab595b3047cb091a558fb5a0b25098314a6582
Juan Villela <juan@villela.co>
2022-08-18 13:16:52 -0400

    fix: Change party mode element to button for better accessibility.

--- assets/scripts/party-mode.ts
+++ assets/scripts/party-mode.ts
@@ -1,12 +1,12 @@
 const body: HTMLBodyElement | null = document.querySelector('body');
-const gandalf: HTMLImageElement | null =
-  document.querySelector('img.party-mode');
+const partyMode: HTMLButtonElement | null =
+  document.querySelector('button.party-mode');
 let isPartyTime: boolean = false;
 
-if (gandalf) {
-  gandalf.addEventListener('click', () => {
+if (partyMode) {
+  partyMode.addEventListener('click', () => {
     body?.setAttribute('data-party', !isPartyTime);
-    gandalf.setAttribute('data-active', !isPartyTime);
+    partyMode.setAttribute('data-active', !isPartyTime);
     isPartyTime = !isPartyTime;
   });
 }
