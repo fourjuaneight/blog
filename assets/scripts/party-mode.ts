9425cb06a9965d2645f9e0efe6f5e32a8783d38d
Juan Villela <juan@villela.co>
2022-08-19 11:53:27 -0400

    fix: Check party mode session storage value and without coercing value.
    When explicitly coercing to a boolean, it will always return true if present as the value is a string.

--- assets/scripts/party-mode.ts
+++ assets/scripts/party-mode.ts
@@ -3,7 +3,7 @@ const body: HTMLBodyElement | null = document.querySelector('body');
 const partyMode: HTMLButtonElement | null =
   document.querySelector('button.party-mode');
 const currentParty: string | null = sessionStorage.getItem(storageLabel);
-let isPartyTime: boolean = Boolean(currentParty);
+let isPartyTime: boolean = currentParty === 'true';
 
 body?.setAttribute('data-party', isPartyTime);
 partyMode?.setAttribute('data-active', isPartyTime);
