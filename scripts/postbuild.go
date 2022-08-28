82456af4e26ffcaf545ae7f1333395ca4677f3be
Juan Villela <juan@villela.co>
2022-08-28 18:18:07 -0400

    feat: Move all build script to single go file.
    I still haven't figured out how to run individual files are separate scripts, so this will have to do for now.

--- scripts/postbuild.go
+++ scripts/postbuild.go
@@ -9,6 +9,10 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/samber/lo"
 	lop "github.com/samber/lo/parallel"
@@ -161,6 +165,7 @@ func mtg(data []Card, dir string) {
 	if err != nil {
 		log.Fatal("[io.Getwd]:", err)
 	}
+	wd = filepath.Join(wd, "..")
 	dist = filepath.Join(wd, "assets/img/", dir)
 
 	// create the directory if it doesn't exist
@@ -192,6 +197,7 @@ func shelf(data []Shelf, dir string) {
 	if err != nil {
 		log.Fatal("[io.Getwd]:", err)
 	}
+	wd = filepath.Join(wd, "..")
 	dist = filepath.Join(wd, "assets/img/", dir)
 
 	// create the directory if it doesn't exist
@@ -210,11 +216,88 @@ func shelf(data []Shelf, dir string) {
 	})
 }
 
-func init() {
-	// save images
-	mtgData := getCardList()
-	mtg(mtgData, "mtg")
+func remoteImages(run bool) {
+	if run {
+		// save images
+		mtgData := getCardList()
+		mtg(mtgData, "mtg")
 
-	shelfData := getShelfList()
-	shelf(shelfData, "shelf")
+		shelfData := getShelfList()
+		shelf(shelfData, "shelf")
+	}
+}
+
+func cleanScripts(run bool) {
+	if run {
+		var files []string
+		// runtime variables
+		site := "https://cleverlaziness.xyz"
+		now := time.Now()
+		unix := now.Unix()
+
+		// create src directory
+		wd, err := os.Getwd()
+		if err != nil {
+			log.Fatal("[io.Getwd]:", err)
+		}
+		// go up one directory
+		wd = filepath.Join(wd, "..")
+		dist := filepath.Join(wd, "dist/")
+
+		// create filter patterns
+		typeIgnore := regexp.MustCompile(`.*(noise|sw).*\.js$`)
+		typeMatch := regexp.MustCompile(`.*\.(js|css|woff|woff2)$`)
+
+		// get files
+		filepath.Walk(dist, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				log.Fatal("[filepath.Glob]:", err)
+			}
+			// filter files
+			ignore := typeIgnore.MatchString(path)
+			matches := typeMatch.MatchString(path)
+
+			// only save matches
+			if !info.IsDir() && matches && !ignore {
+				files = append(files, path)
+			}
+
+			return nil
+		})
+		files = lo.Map(files, func(item string, _ int) string {
+			clean := strings.Replace(item, dist, "", -1)
+			quotes := `"` + clean + `"`
+
+			return quotes
+		})
+
+		// get service worker
+		swRaw, err := os.ReadFile(filepath.Join(dist, "sw.js"))
+		if err != nil {
+			log.Fatal("[os.ReadFile]:", err)
+		}
+		sw := string(swRaw)
+
+		// replace content
+		assets := regexp.MustCompile(`\["staticAssets"\]`)
+		baseURL := regexp.MustCompile(`baseURL`)
+		version := regexp.MustCompile(`"version"`)
+		swAssets := assets.ReplaceAllString(sw, "["+strings.Join(files, ",")+"]")
+		swSite := baseURL.ReplaceAllString(swAssets, site)
+		fmtSW := version.ReplaceAllString(swSite, `"`+strconv.FormatInt(unix, 10)+`"`)
+
+		// replace sw.js
+		newSW, err := os.Create(filepath.Join(dist, "sw.js"))
+		if err != nil {
+			log.Fatal("[os.Create]:", err)
+		}
+		defer newSW.Close()
+
+		newSW.WriteString(fmtSW)
+	}
+}
+
+func main() {
+	remoteImages(false)
+	cleanScripts(true)
 }
